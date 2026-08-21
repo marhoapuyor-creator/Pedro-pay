5ccf7640d114ffe59341b7a2856b681b5d0615ee
marhoapuyor-creator <marhoapuyor@gmail.com>
2026-08-22 00:34:45 +0100

    Fix app. Listen indentation

--- server/index.js
+++ server/index.js
@@ -10,7 +10,10 @@ app.get('/', (req, res) => {
 app.get('/users', (req, res) => {
   res.json({ message: "Users endpoint dey work!" })
 })
-
 app.get('/payments', (req, res) => {
   res.json({ message: "Payments endpoint dey work!" })
+})
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Pedro Pay API running on port ${PORT}`)
 })
